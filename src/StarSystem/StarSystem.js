da8daba06ef110e2fa6b6ab736c727a63f686912
jmuzsik <jerrymuzsik@icloud.com>
2018-11-29 02:10:58 -0500

    why not

--- src/StarSystem/StarSystem.js
+++ src/StarSystem/StarSystem.js
@@ -10,25 +10,69 @@ export default class StarSystem extends Component {
   };
   componentDidMount() {
     createStarSystem();
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      setTimeout(() => {
+        document.querySelector('.typewriter .first').classList.remove('hidden');
+      }, 1000);
+      setTimeout(() => {
+        document
+          .querySelector('.typewriter .second')
+          .classList.remove('hidden');
+      }, 3500);
+      setTimeout(() => {
+        document.querySelector('.typewriter .third').classList.remove('hidden');
+      }, 6300);
+      setTimeout(() => {
+        document
+          .querySelector('.typewriter .fourth')
+          .classList.remove('hidden');
+      }, 9200);
+      setTimeout(() => {
+        document.querySelector('.typewriter .fifth').classList.remove('hidden');
+      }, 11200);
+      setTimeout(() => {
+        document.querySelector('.typewriter .sixth').classList.remove('hidden');
+      }, 12500);
+      setTimeout(() => {
+        document
+          .querySelector('.typewriter .seventh')
+          .classList.remove('hidden');
+      }, 16000);
+      setTimeout(() => {
+        document
+          .querySelector('.typewriter .eighth')
+          .classList.remove('hidden');
+      }, 18000);
+    } else {
+      document
+        .querySelectorAll('hidden')
+        .forEach(el => el.classList.remove('hidden'));
+    }
   }
   render() {
     return (
       <div id="star-system">
         <div className="container">
           <canvas className="canvas" />
-          <h1 className="first">Jerry Muzsik</h1>
-          <h3 className="second">A remote developer</h3>
-          <p className="third">Front end, back end, I do it all</p>
-          <p className="fourth">Check me out on:</p>
-          <a className="fifth" href="https://github.com/jmuzsik">
-            GitHub
-          </a>
-          <p className="sixth">I write a lot of code</p>
-          <p className="seventh">Everything else is extra.</p>
-          <p className="eighth">Godspeed</p>
-          <h2 className="landscape">
-            Uh oh, thing's broke. Get back to portrait.
-          </h2>
+          <div className="typewriter">
+            <h1 className="first hidden">Jerry Muzsik</h1>
+            <h3 className="second hidden">A remote developer</h3>
+            <p className="third hidden">Web, Mobile, everything</p>
+            <p className="fourth hidden">Check me out on:</p>
+            <a className="fifth hidden" href="https://github.com/jmuzsik">
+              GitHub
+            </a>
+            <p className="sixth hidden">Always looking for adventure</p>
+            <p className="seventh hidden">Sometimes work</p>
+            <p className="eighth hidden">Godspeed</p>
+            <h2 className="landscape">
+              Uh oh, thing's broke. Get back to portrait.
+            </h2>
+          </div>
         </div>
       </div>
     );
